e759d4886182815a70d9e35a0ebe63c7533a2fcb
Mohamed KHELIFI <mohamedchrif.khelifi@gmail.com>
2018-10-11 23:42:14 +0200

    add granted authorities to MyUserDetailsService

--- src/main/java/com/medkhelifi/tutorials/todolist/services/MyUserDetailsService.java
+++ src/main/java/com/medkhelifi/tutorials/todolist/services/MyUserDetailsService.java
@@ -1,13 +1,19 @@
 package com.medkhelifi.tutorials.todolist.services;
 
 import com.medkhelifi.tutorials.todolist.models.dao.UserDao;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.medkhelifi.tutorials.todolist.models.entities.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import org.springframework.security.core.userdetails.User;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Transactional
 public class MyUserDetailsService  implements UserDetailsService {
@@ -17,15 +23,23 @@ public class MyUserDetailsService  implements UserDetailsService {
     @Transactional
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         com.medkhelifi.tutorials.todolist.models.entities.User user   = userDao.findByUsername(s);
+        List<GrantedAuthority> grantedAuthorities = buildAuthoritiesForUser(user.getRoleByRoleId());
         if(user == null){
             throw new UsernameNotFoundException(s);
         }else{
-            return buildUserForAuthentification(user);
+            return buildUserForAuthentification(user, grantedAuthorities);
         }
     }
 
-    private User buildUserForAuthentification(com.medkhelifi.tutorials.todolist.models.entities.User user){
-        return new User(user.getUsername(), user.getPassword(), null);
+    private List<GrantedAuthority> buildAuthoritiesForUser(Role role) {
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(role.getName());
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>(Collections.singleton(simpleGrantedAuthority));
+        return grantedAuthorities;
+
+    }
+
+    private User buildUserForAuthentification(com.medkhelifi.tutorials.todolist.models.entities.User user, List<GrantedAuthority> grantedAuthorities){
+        return new User(user.getUsername(), user.getPassword(), grantedAuthorities);
     }
 
     public void setUserDao(UserDao userDao) {
